7f1469f37b1bed8d2df06a13f05c74ebb5ae31f5
AshIdiot <saponaramatteo30@gmail.com>
2026-01-28 12:47:37 -0500

    fd

---
+++ Assets/InClassExamples/2026/Rat.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class Rat : MonoBehaviour
+{
+    [SerializeField] private Animator ani;
+
+    private void Update()
+    {
+        if (Input.GetAxisRaw("Horizontal") != 0)
+        {
+
+        }
+    }
+
+}
