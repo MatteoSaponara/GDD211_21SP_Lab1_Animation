a5d7210f0a3da499cce94e6ad27011adec876802
AshIdiot <saponaramatteo30@gmail.com>
2026-01-28 13:08:31 -0500

    df
    ds

--- Assets/InClassExamples/2026/Rat.cs
+++ Assets/InClassExamples/2026/Rat.cs
@@ -1,12 +1,23 @@
 using UnityEngine;
 
+RequireComponent
 public class Rat : MonoBehaviour
 {
     [SerializeField] private Animator ani;
 
     private void Update()
     {
-        if (Input.GetAxisRaw("Horizontal") != 0)
+        if (Input.GetAxisRaw("Horizontal") > 0) // Right
+        {
+            transform.position += new Vector3(1, 0, 0) * Time.deltaTime;
+            ani.Play("RatWalkingRight");
+        }
+        else if (Input.GetAxisRaw("Horizontal") < 0) // Left
+        {
+            transform.position += new Vector3(-1, 0, 0) * Time.deltaTime;
+            ani.Play("RatWalkingLeft");
+        }
+        else // nothing
         {
 
         }
